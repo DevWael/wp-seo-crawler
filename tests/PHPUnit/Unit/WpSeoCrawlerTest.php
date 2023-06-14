77df2d3e4660a4117de994e5e82e375ad3fa6a34
devwael <dev.ahmedwael@gmail.com>
2023-06-14 04:27:59 +0300

    update test case "test_wp_seo_crawler_instance"

--- tests/PHPUnit/Unit/WpSeoCrawlerTest.php
+++ tests/PHPUnit/Unit/WpSeoCrawlerTest.php
@@ -3,9 +3,14 @@
 namespace WpSeoCrawler\Tests\Unit;
 
 use DevWael\WpSeoCrawler\WpSeoCrawler;
+use Brain\Monkey\Functions;
 
 class WpSeoCrawlerTest extends AbstractUnitTestCase {
 	public function test_wp_seo_crawler_instance() {
+		Functions\expect( 'esc_url' )->andReturnFirstArg();
+		Functions\expect( 'home_url' );
+		Functions\expect( 'is_admin' )->andReturn( true );
+
 		$this->assertInstanceOf( WpSeoCrawler::class, WpSeoCrawler::instance() );
 	}
 }
